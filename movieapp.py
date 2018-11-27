13b6e9b4749d0336a1c7a8b03dbcfcf64c34536b
zhangruochi <zrc720@gmail.com>
2018-11-27 05:35:55 -0500

    2018-11-27

--- movieapp.py
+++ movieapp.py
@@ -1,50 +1,81 @@
-from flask import Flask
-from flask import render_template
-from flask_bootstrap import Bootstrap
+import web
+import pymysql
+from web import form
+from web.contrib.template import render_jinja
 
-from datetime import datetime
-
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-
-@app.route("/")
-@app.route("/index")
-def index():
-    return render_template("index.html")
-
-@app.route("/movielist.html")
-def movielist():
-    return render_template("movie_list.html")
-
-@app.route("/moviedetail.html")
-def moviedetail():
-    return render_template("movie_detail.html")
-
-@app.route("/actor.html")
-def actor():
-    return render_template("actor.html")
-
-@app.route("/director.html")
-def director():
-    return render_template("actor.html")
-
-@app.route("/onshow.html")
-def onshow():
-    return render_template("on_show.html")
-
-@app.route("/user.html")
-def user():
-    return render_template("account.html")
+web.config.debug = False
+pymysql.install_as_MySQLdb()
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"),400
+urls = (
+    '/','Index',
+    '/index', 'Index',
+    '/login','Login',
+    '/logout','Logout',
+    '/onshow','Onshow',
+    '/user',"User"
+)
+db = web.database(dbn='mysql', user='root', pw='lv23623600', db='movie_infor')
+app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore('sessions'),initializer={'logged_in': False})
 
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template("500.html"),500
+
+render = web.template.render('templates/',globals={'context':session})
 
 
-if __name__ == '__main__':
-    app.run()        
+class Index:
+    def GET(self):
+        return render.index()
+
+
+class Login:
+    def GET(self):
+        return render.login()
+
+    def POST(self):
+        i = web.input()
+        email = i.get('email')
+        passwd = i.get('passwd')
+        check = db.query('select * from user where account = $email and password = $passwd', vars = {'email':email, 'passwd': passwd})
+        if check:
+            session.logged_in = True
+            session.user = email
+            return render.user(session.user)
+        else:
+            return render.login()
+
+class Onshow:
+    def GET(self):
+        onshow_movies = db.select("on_show")
+        return render.onshow(onshow_movies)
+
+
+class Logout:
+    def GET(self):
+        session.logged_in = False
+        session.user = None
+        raise web.seeother("/")
+
+
+class User:
+    def GET(self):
+        if not session.logged_in:
+            return render.login()
+        else:
+            return render.user(session.user)
+
+    def POST(self):
+        i = web.input()
+        key = i.get("key")
+        res = db.query('select * from actor where name = $key',vars = {"key":key})
+        if res:
+            return render.actor(res)
+        else:
+            return "can not find"
+
+
+
+
+
+if __name__ == "__main__": 
+    app.run()
